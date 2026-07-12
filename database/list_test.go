1d93889d1f6a8c1b0ee7933d0241d415c15291d6
Peter Todorov <petertodorov@Peters-MacBook-Air.local>
2026-07-12 13:49:13 +0300

    tests: add tests for Lpush and RPush

---
+++ database/list_test.go
@@ -0,0 +1,65 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRPush(t *testing.T) {
+	db := CreateDatabase()
+	length, err := db.RPush("test", []string{"test"})
+	assert.Len(t, db.data["test"], 1)
+	assert.Equal(t, 1, length)
+	assert.Nil(t, err)
+
+	length, err = db.RPush("test", []string{"test", "test"})
+	assert.Len(t, db.data["test"], 3)
+	assert.Equal(t, 3, length)
+	assert.Nil(t, err)
+
+	db.data = map[string]Value{
+		"key": StringValue("value"),
+	}
+
+	length, err = db.RPush("key", []string{"test"})
+	assert.Equal(t, 0, length)
+	assert.Error(t, err)
+
+	db.data = map[string]Value{
+		"key": ListValue{"test"},
+	}
+
+	db.RPush("key", []string{"test1"})
+
+	assert.Equal(t, ListValue{"test", "test1"}, db.data["key"])
+}
+
+func TestLPush(t *testing.T) {
+	db := CreateDatabase()
+	length, err := db.RPush("test", []string{"test"})
+	assert.Len(t, db.data["test"], 1)
+	assert.Equal(t, 1, length)
+	assert.Nil(t, err)
+
+	length, err = db.RPush("test", []string{"test", "test"})
+	assert.Len(t, db.data["test"], 3)
+	assert.Equal(t, 3, length)
+	assert.Nil(t, err)
+
+	db.data = map[string]Value{
+		"key": StringValue("value"),
+	}
+
+	length, err = db.RPush("key", []string{"test"})
+	assert.Equal(t, 0, length)
+	assert.Error(t, err)
+
+	db.data = map[string]Value{
+		"key": ListValue{"test"},
+	}
+
+	db.LPush("key", []string{"test1"})
+
+	assert.Equal(t, ListValue{"test1", "test"}, db.data["key"])
+}
