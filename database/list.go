3020c2092bd981fd4e3f05874912afb02747d538
Peter Todorov <petertodorov@Peters-MacBook-Air.local>
2026-07-12 13:49:34 +0300

    feat: add Lpush

--- database/list.go
+++ database/list.go
@@ -28,3 +28,20 @@ func (db *Database) RPush(key string, values []string) (int, error) {
 
 	return len(typeData), nil
 }
+
+func (db *Database) LPush(key string, values []string) (int, error) {
+	if _, ok := db.data[key]; !ok {
+		db.data[key] = make(ListValue, 0)
+	} else if db.data[key].GetType() != list {
+		return 0, fmt.Errorf(wrongDatatype)
+	}
+
+	data := db.data[key]
+	typeData := data.(ListValue)
+	for _, value := range values {
+		typeData = append([]string{value}, typeData...)
+		db.data[key] = typeData
+	}
+
+	return len(typeData), nil
+}
